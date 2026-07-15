91b1652888caf39959627da5cb0e02271ea7a861
JLouceiro <jrlouceiro@live.com>
2026-07-15 16:46:56 +0100

    chore: remove preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 export default defineConfig({
   site: "https://jlouceiro.netlify.app/",
-  integrations: [preact()],
 });
